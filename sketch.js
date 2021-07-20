1e3460f1405dfc06566039a3283c75b56118951b
TheGrimCreeper <87707886+TheGrimCreeper@users.noreply.github.com>
2021-07-20 19:01:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var box;
+
+function setup() {
+  createCanvas(1000,1000);
+  box = createSprite(108,250,365,310);
+
+}
+
+function draw() 
+{
+   background("yellow");
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    box.position.x = box.position.x +5;
+  }
+
+    if (keyIsDown(LEFT_ARROW)) 
+  {
+    box.position.x= box.position.x -5;
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    box.position.y = box.position.y - 5;
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    box.position.y = box.position.y + 5;
+  }
+
+  drawSprites();
+}
+
+
+
+
